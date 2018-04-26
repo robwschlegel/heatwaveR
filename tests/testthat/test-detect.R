9084007c3fedf85622042aafc94fc63c8ca9bfae
Robert William Schlegel <robwschlegel@gmail.com>
2018-04-26 12:45:27 +0200

    Testing framework for other functions

---
+++ tests/testthat/test-detect.R
@@ -0,0 +1,5 @@
+context("test-detect.R")
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
---
+++ tests/testthat/test-geoms.R
@@ -0,0 +1,5 @@
+context("test-geoms.R")
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
