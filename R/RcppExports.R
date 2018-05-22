cd07aa70cb580118a788e2f5a647479a70ab7301
AJ Smit <albertus.smit@gmail.com>
2018-05-22 20:43:51 +0200

    Minor.

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 clim_calc_cpp <- function(x, windowHalfWidth, pctile) {
-    .Call(`_heatwaveR_clim_calc_cpp`, x, windowHalfWidth, pctile)
+    .Call('_heatwaveR_clim_calc_cpp', PACKAGE = 'heatwaveR', x, windowHalfWidth, pctile)
 }
 
--- tests/testthat/test-clim_calc.R
+++ tests/testthat/test-clim_calc.R
@@ -8,7 +8,7 @@ test_that("clim_calc() returns the correct output", {
   ts_wide <- heatwaveR:::clim_spread(ts_whole, clim_start = "1983-01-01",
                                      clim_end = "2012-12-31", windowHalfWidth = 5)
   res <- heatwaveR:::clim_calc(ts_wide, windowHalfWidth = 5, pctile = 90)
-  expect_is(res, "data.frame")
+  expect_is(res, "matrix")
   expect_equal(ncol(res), 4)
   expect_equal(nrow(res), 366)
 })
