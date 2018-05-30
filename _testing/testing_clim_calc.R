4e4ab1f954825caa9c279fdcf03bb373a4479baf
AJ Smit <albertus.smit@gmail.com>
2018-05-30 06:09:25 +0200

    Miscellaneous formatting changes.

--- _testing/testing_clim_calc.R
+++ _testing/testing_clim_calc.R
@@ -5,10 +5,6 @@ library(microbenchmark)
 library(data.table)
 library(imputeTS)
 
-data <- sst_WA
-ts_x <- data$t
-ts_y <- data$temp
-ts_xy <- data.table(ts_x, ts_y)
 
 climatologyPeriod <- c("1983-01-01", "2012-12-31")
 maxPadLength <- 1
@@ -20,42 +16,42 @@ clmOnly <- TRUE
 clim_start <- "1983-01-01"
 clim_end <- "2012-12-31"
 
-ts_whole <- make_whole_fast(ts_xy, x = ts_x, y = ts_y)
+res_clim <- ts2clm(sst_WA, climatologyPeriod = c("1983-01-01", "2012-12-31"))
 
-# ts_wide <- clim_spread(ts_whole, clim_start, clim_end, windowHalfWidth)
+minDuration <- 5
+maxGap <- 2
 
-.NA2mean <- function(x) {
-  z <- round(mean(x, na.rm = TRUE), 2)
-  x[is.na(x)] <- z
-  return(x)
-}
+data <- res_clim
+ts_x <- data$t
+ts_y <- data$temp
+ts_seas <- data$seas
+ts_thresh <- data$thresh
+t_series <- data.table::data.table(ts_x, ts_y, ts_seas, ts_thresh)
 
-data <- ts_whole
+t_series$ts_y[is.na(t_series$ts_y)] <- t_series$ts_seas[is.na(t_series$ts_y)]
+t_series$threshCriterion <- as.numeric(t_series$ts_y > t_series$ts_thresh)
 
-ts_whole <- data[ts_x %between% c(clim_start, clim_end)]
-data.table::setDT(ts_whole)[, ts_x := format(as.Date(ts_x), "%Y") ]
-ts_spread <- data.table::dcast(ts_whole, doy ~ ts_x, value.var = "ts_y")
-ts_spread_sub <- data.table::data.table((sapply(ts_spread[59:61, ], function(x) .NA2mean(x))))
-ts_spread[60, ] <- ts_spread_sub[2, ]
+# proto_1 <- proto_event(t_series, criterion_column = 5, minDuration = minDuration,
+#                        maxGap = maxGap)
 
-begin_pad <- utils::tail(ts_spread, windowHalfWidth)
-end_pad <- utils::head(ts_spread, windowHalfWidth)
-l <- list(begin_pad, ts_spread, end_pad)
-ts_spread <- data.table::rbindlist(l)
-rm(begin_pad); rm(end_pad); rm(l)
+criterion_column <- 5
 
-
+t_series <- as.data.frame(t_series)
+ex1 <- rle(as.vector(t_series[, get(criterion_column)])) ###
+ind1 <- rep(seq_along(ex1$lengths), ex1$lengths)
+s1 <- split(zoo::index(t_series[, get(criterion_column)]), ind1)
 
 
-# ind <- which(ts_whole$ts_y %in% sample(ts_whole$ts_y, 100))[,2]
-# ts_whole$ts_y[ind] <- NA
+
+
+
 
 # benchmarks --------------------------------------------------------------
 
 
 
 profvis(res_climF <- ts2clm(sst_WA, climatologyPeriod = c("1983-01-01", "2012-12-31"), robust = FALSE))
-out <- detect_event(res_climF)
+profvis(out <- detect_event(res_climF))
 
 
 microbenchmark(ts2clm(sst_WA, climatologyPeriod = c("1983-01-01", "2012-12-31"), robust = TRUE),
