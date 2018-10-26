34ff02fe965d80d8ad953be8327038965cfea3c7
Robert William Schlegel <robwschlegel@gmail.com>
2018-10-26 12:32:03 -0300

    Edit

--- tests/testthat/test-smooth_percentile.R
+++ tests/testthat/test-smooth_percentile.R
@@ -8,7 +8,7 @@ test_that("smooth_percentile() returns the correct output", {
   ts_wide <- heatwaveR:::clim_spread(ts_whole, clim_start = "1983-01-01",
                                      clim_end = "2012-12-31", windowHalfWidth = 5)
   ts_calc <- heatwaveR:::clim_calc(ts_wide, windowHalfWidth = 5, pctile = 90)
-  res <- heatwaveR:::smooth_percentile(ts_calc, smoothPercentileWidth = 31)
+  res <- heatwaveR:::smooth_percentile(ts_calc, smoothPercentileWidth = 31, var_calc = F)
   expect_is(res, "data.frame")
   expect_equal(ncol(res), 3)
   expect_equal(nrow(res), 366)
